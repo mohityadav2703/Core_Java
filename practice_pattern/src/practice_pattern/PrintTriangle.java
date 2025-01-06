d2d29d20fcb37550951c2e2e9df675b172abae35
Mohit Kumar <143256804+yadav-mohit-17@users.noreply.github.com>
2025-01-06 10:57:13 +0530

    update

---
+++ practice_pattern/src/practice_pattern/PrintTriangle.java
@@ -0,0 +1,17 @@
+package practice_pattern;
+
+public class PrintTriangle {
+
+	public static void main(String[] args) {
+		int star=5;
+		
+		for(int i=1;i<=star;i++) {
+			
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
